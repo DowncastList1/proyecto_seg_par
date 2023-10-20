6d3e9b4de1d4e5029ab658f7991693df80aefeb2
Julatl05 <133040269+Julatl05@users.noreply.github.com>
2023-10-20 10:01:12 -0600

    Update spaceinvaders.py

--- Recursos/Space_invaders_comment/spaceinvaders.py
+++ Recursos/Space_invaders_comment/spaceinvaders.py
@@ -101,12 +101,15 @@ class Enemy(sprite.Sprite):
         self.images.append(transform.scale(img1, (40, 35)))
         self.images.append(transform.scale(img2, (40, 35)))
 
-
+#Se crea la clase EnemiesGroup con el parametro sprite.Group
 class EnemiesGroup(sprite.Group):
+    '''Constructo que inicializar el grupo, le damos los parametros de columnas(colums) y filas (rows)
+        Es llamada cuando se crea una nueva instancia de la clase EnemiesGrou'''
     def __init__(self, columns, rows):
+        #Se establecen varios atributos para el movimiento,tiempo y posicion de los enemigos
         sprite.Group.__init__(self)
         self.enemies = [[None] * columns for _ in range(rows)]
-        self.columns = columns
+        self.columns = columns 
         self.rows = rows
         self.leftAddMove = 0
         self.rightAddMove = 0
@@ -121,7 +124,9 @@ class EnemiesGroup(sprite.Group):
         self._leftAliveColumn = 0
         self._rightAliveColumn = columns - 1
 
+    #Funcion que actualiza la posicion de los enemigos, le damos el parametro de current_time
     def update(self, current_time):
+        #Se actualiza la pocision en base al tiempo actual del juego 
         if current_time - self.timer > self.moveTime:
             if self.direction == 1:
                 max_move = self.rightMoves + self.rightAddMove
@@ -147,46 +152,59 @@ class EnemiesGroup(sprite.Group):
                 self.moveNumber += 1
 
             self.timer += self.moveTime
-
+    
+    '''Funcion para agregar enemigos a la instancia de la clase
+    Tambien almacena a los enemigos en una matriz llamada self.enemies'''
     def add_internal(self, *sprites):
         super(EnemiesGroup, self).add_internal(*sprites)
         for s in sprites:
             self.enemies[s.row][s.column] = s
 
+    '''Funcion para eleminar enemigos a la instancia de la clase tambien 
+    los elimina de la mariz'''
     def remove_internal(self, *sprites):
         super(EnemiesGroup, self).remove_internal(*sprites)
         for s in sprites:
             self.kill(s)
         self.update_speed()
 
+
+    '''Verifica si una columna esta vacia (Todos los enemigis han sido eliminados)
+    en kla matriz self.enemies'''
     def is_column_dead(self, column):
         return not any(self.enemies[row][column]
                        for row in range(self.rows))
 
+    #Devuelve un enemigo
     def random_bottom(self):
         col = choice(self._aliveColumns)
         col_enemies = (self.enemies[row - 1][col]
                        for row in range(self.rows, 0, -1))
         return next((en for en in col_enemies if en is not None), None)
 
+    #Cambia la velocidad de los enemigos segun los enemigos elminados
     def update_speed(self):
         if len(self) == 1:
             self.moveTime = 200
         elif len(self) <= 10:
             self.moveTime = 400
 
+    #Sirve para actualizar cuando se mata a un enemigo 
     def kill(self, enemy):
+        '''Actualiza la matriz de los enemigos estableciendo la posicion 
+        de ek enemigo a None'''
         self.enemies[enemy.row][enemy.column] = None
         is_column_dead = self.is_column_dead(enemy.column)
         if is_column_dead:
             self._aliveColumns.remove(enemy.column)
 
+        #Verifica si la columna a la derecha esta vacio 
         if enemy.column == self._rightAliveColumn:
             while self._rightAliveColumn > 0 and is_column_dead:
                 self._rightAliveColumn -= 1
                 self.rightAddMove += 5
                 is_column_dead = self.is_column_dead(self._rightAliveColumn)
-
+        #Verifica si la columna a la izquierda esta vacia
         elif enemy.column == self._leftAliveColumn:
             while self._leftAliveColumn < self.columns and is_column_dead:
                 self._leftAliveColumn += 1
@@ -637,7 +655,6 @@ class SpaceInvaders(object):
 
             display.update()
             self.clock.tick(60)
-
 
 if __name__ == '__main__':
     game = SpaceInvaders()
