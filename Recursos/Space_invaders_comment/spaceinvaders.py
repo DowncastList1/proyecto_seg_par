1bdecc7f741df293d8c735d0fa7f347729192f2d
MayrinReyes <129233059+MayrinReyes@users.noreply.github.com>
2023-10-20 10:57:18 -0600

    Update spaceinvaders.py

--- Recursos/Space_invaders_comment/spaceinvaders.py
+++ Recursos/Space_invaders_comment/spaceinvaders.py
@@ -7,7 +7,7 @@ from pygame import *
 import sys
 from os.path import abspath, dirname
 from random import choice
-
+##desde donde se buscan los archivos y en que carpeta se va a buscar 
 BASE_PATH = abspath(dirname(__file__))
 FONT_PATH = BASE_PATH + '/fonts/'
 IMAGE_PATH = BASE_PATH + '/images/'
